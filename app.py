0b1b1d11c2a099073c7e03a6716ec367a22b6d82
Robim-rur <cruvinelebook@gmail.com>
2026-05-26 15:51:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -28,8 +28,8 @@ st.set_page_config(
 # PARÂMETROS
 # =========================================================
 
-GAIN_FIXO = 0.02
-LOSS_FIXO = 0.02
+GAIN_FIXO = 0.04
+LOSS_FIXO = 0.06
 
 ADX_MINIMO = 20
 
